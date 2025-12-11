32a8144fbef44a78368d7db398433ae0be0414ed
Jakub Wartak <jakub.wartak@enterprisedb.com>
2025-12-11 13:39:11 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # tpch-vs-pg_plan_advise
+
+The idea is to run whole suite of real-world queries (e.g. TPC-H) , collect plan_advices
+and feed them back to the pg_plan_advice and see which one were used and which couldn't
+be used due to some conflict/incapability/duplicates or bugs if any.
+
+prepare:
+createdb dbt3
+bunzip2 dbt3_tpch_1GB.sql.bz2
+import dbt3_tpch_1GB_dump to dbt3 postgresdb using pg_restore
+alter system set shared_preload_libraries = 'pg_plan_advice';
+pg_ctl restart
+
+The below will try to find advices that were not found
+run: ./analyzeit.sh 
+
---
+++ analyzeit.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# to run just one qXX, uncomment the next line, results will be in /tmp/advice
+# and /tmp/result
+#for F in `ls -1 q*.sql | sort -n`; do
+for F in q10.sql; do
+	echo $F;
+
+	# get advice from explain
+	echo 'explain (plan_advice, timing off, costs off, settings off, memory off, format json)' > tmp.sql
+	cat $F >> tmp.sql
+	ADVICE=`psql -Antb -p 5432 -h /tmp dbt3 -f tmp.sql | jq -sr '.[].[]."Generated Plan Advice"'`
+	#echo -e "$F: got $ADVICE"
+
+	# get what was matched
+	echo -e "set pg_plan_advice.advice = '$ADVICE';" > tmp.sql;
+	echo 'explain (plan_advice, timing off, costs off, settings off, memory off, format json)' >> tmp.sql
+	cat $F >> tmp.sql
+	RESULT=`psql -Antb -p 5432 -h /tmp dbt3 -f tmp.sql | grep -v ^SET | jq -sr '.[].[]."Supplied Plan Advice"'`
+	#echo "$F: got $RESULT"
+
+	echo "$ADVICE" > /tmp/advice
+	echo "$RESULT" > /tmp/result
+
+	#diff -u /tmp/advice /tmp/result
+	# sed for identing whole output for easier readability:
+	grep -v \
+		-e '\/\* matched \*\/' \
+		-e '\/\* matched, conflicting \*\/' \
+		/tmp/result | sed 's/^/\t/'
+
+done
